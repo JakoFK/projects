842920d37ae34fa0e19f8b7b11a23ec694072152
Gabriel <lenz.5gabriel@gmail.com>
2025-08-21 17:45:31 +0200

    feat: add layout and screens for settings tab in BucketList application

--- Others/Mobile Applications/BucketList/app/_layout.tsx
+++ Others/Mobile Applications/BucketList/app/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
+import Index from "./(tabs)";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen 
+        name="index"
+        options={{ title: "Settings" }}
+      />
+      <Stack.Screen 
+        name="(tabs)"
+        options={{ headerShown: false }}
+        />
+    </Stack>
+  );
 }
